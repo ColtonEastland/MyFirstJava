173b3fa77d5c421871d24a0429c735265831a058
Colton <21317512+ColtonEastland@users.noreply.github.com>
2021-03-09 15:27:32 -0600

    First Commit

---
+++ MyFirstJava.java
@@ -0,0 +1,12 @@
+public class MyFirstJava
+{
+	public void print()
+	{
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+	MyFirstJava myFirstObj = new MyFirstJava();
+	myFirstObj.print();
+	}
+}
